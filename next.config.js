98fee09b685aef395b23e0bcc1b446d04684f8cd
Jan Biasi <jan.biasi@sgkbo365.onmicrosoft.com>
2022-02-17 10:48:45 +0100

    ci: add custom build id namespace

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const { v5 } = require('uuid');
+
+const BUILD_ID_NAMESPACE = process.env.BUILD_ID_NAMESPACE;
+
+if (!BUILD_ID_NAMESPACE) {
+	throw new Error('BUILD_ID_NAMESPACE environment variable is not defined');
 }
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	poweredByHeader: false,
+	generateBuildId: async () => {
+		return v5(`${Date.now()}`, BUILD_ID_NAMESPACE);
+	},
+};
+
+module.exports = nextConfig;
