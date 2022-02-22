56616d66bd6795b9abad8d71f38ba1eb51e947bb
Jan Biasi <jan.biasi@sgkbo365.onmicrosoft.com>
2022-02-23 00:07:00 +0100

    feat: add graphql integration

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,11 +1,32 @@
-import { GraphQLClient } from 'graphql-request';
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache, concat } from '@apollo/client';
 
 if (!process.env.NEXT_PUBLIC_CMS_GRAPHQL_API_URL) {
 	throw new Error('CMS_GRAPHQL_API_URL environment variable is not defined');
 }
 
-export const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_CMS_GRAPHQL_API_URL}`, {
-	mode: 'cors',
-	keepalive: true,
-	timeout: 20 * 1000,
+const httpLink = new HttpLink({
+	uri: process.env.NEXT_PUBLIC_CMS_GRAPHQL_API_URL,
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+	operation.setContext(({ headers = {} }) => ({
+		headers: {
+			...headers,
+			'Cockpit-Token': process.env.NEXT_PUBLIC_CMS_API_TOKEN,
+		},
+	}));
+
+	return forward(operation);
+});
+
+// export const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_CMS_GRAPHQL_API_URL}`, {
+// 	mode: 'cors',
+// 	keepalive: true,
+// 	timeout: 20 * 1000,
+// });
+
+export const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: concat(authMiddleware, httpLink),
+	connectToDevTools: process.env.NODE_ENV !== 'production',
 });
---
+++ graphql/index.ts
@@ -0,0 +1,2 @@
+export * from './client';
+export * from './compiled/collection';
