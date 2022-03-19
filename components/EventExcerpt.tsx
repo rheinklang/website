832352e0d823ec011e421d42424afc13eaac7644
Jan Biasi <jan.biasi@sgkbo365.onmicrosoft.com>
2022-03-19 17:43:36 +0100

    style: use inter as default font

--- components/EventExcerpt.tsx
+++ components/EventExcerpt.tsx
@@ -6,6 +6,7 @@ import { Badge } from './Badge';
 import { Button } from './Button';
 import { Heading } from './Heading';
 import { StaticRoutes } from '../utils/routes';
+import { useTranslation } from '../hooks/useTranslation';
 
 export interface EventExcerptProps {
 	title: string;
@@ -25,45 +26,51 @@ export const EventExcerpt: FC<EventExcerptProps> = ({
 	location,
 	slug,
 	className,
-}) => (
-	<article className="h-full flex items-start">
-		<div className="w-12 mt-10 flex-shrink-0 flex flex-col text-center leading-none rounded-xl bg-white">
-			<span className="pt-2 pb-2 mb-2 border-b-2 text-gray-300 border-gray-100 uppercase">
-				{format(date, 'MMM')}
-			</span>
-			<span className="pb-2 font-medium text-lg title-font leading-none text-gray-500">{format(date, 'dd')}</span>
-		</div>
-		<div className="flex-grow pl-6">
-			<Badge type="secondary" className="mb-3">
-				{category}
-			</Badge>
-			<Heading level="3" className="mb-1 text-white">
-				{title}
-			</Heading>
-			{/* <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Event Title</h1> */}
-			<p className="leading-relaxed mb-2 text-white">{description}</p>
-			<p className="inline-flex items-center text-sea-green-900">
-				<LocationMarkerIcon className="inline-block h-4 " />
-				<span className="flex-grow flex flex-col pl-3">
-					{location && (
-						<span className="title-font font-medium">
-							{location.name}, {location.country}
-						</span>
-					)}
+}) => {
+	const translate = useTranslation();
+
+	return (
+		<article className="h-full flex items-start">
+			<div className="w-12 mt-10 flex-shrink-0 flex flex-col text-center leading-none rounded-xl bg-white">
+				<span className="pt-2 pb-2 mb-2 border-b-2 text-gray-300 border-gray-100 uppercase">
+					{format(date, 'MMM')}
 				</span>
-			</p>
-			<div className="flex mt-4">
-				<Button
-					type="black"
-					link={{
-						href: `${StaticRoutes.EVENT_DETAIL}/${slug}`,
-						children: 'Details',
-						icon: <ArrowRightIcon className="inline ml-2 h-4" />,
-					}}
-				/>
+				<span className="pb-2 font-medium text-lg title-font leading-none text-gray-500">
+					{format(date, 'dd')}
+				</span>
 			</div>
-		</div>
-	</article>
-);
+			<div className="flex-grow pl-6">
+				<Badge type="secondary" className="mb-3">
+					{translate(`event.type.${category}`)}
+				</Badge>
+				<Heading level="3" className="mb-1 text-white">
+					{title}
+				</Heading>
+				{/* <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Event Title</h1> */}
+				<p className="leading-relaxed mb-2 text-white">{description}</p>
+				<p className="inline-flex items-center text-sea-green-900">
+					<LocationMarkerIcon className="inline-block h-4 " />
+					<span className="flex-grow flex flex-col pl-3">
+						{location && (
+							<span className="title-font font-medium">
+								{location.name}, {location.country}
+							</span>
+						)}
+					</span>
+				</p>
+				<div className="flex mt-4">
+					<Button
+						type="black"
+						link={{
+							href: `${StaticRoutes.EVENT_DETAIL}/${slug}`,
+							children: 'Details',
+							icon: <ArrowRightIcon className="inline ml-2 h-4" />,
+						}}
+					/>
+				</div>
+			</div>
+		</article>
+	);
+};
 
 EventExcerpt.displayName = 'EventExcerpt';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import '../styles/fonts.css';
 import '../styles/globals.css';
 
 function RheinklangApp({ Component, pageProps }: AppProps) {
