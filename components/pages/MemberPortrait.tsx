ff9b8eb1ab999bc5a6fd8cb70c976ceac4e47c92
Jan Biasi <jan.biasi@sgkb.ch>
2023-01-08 15:13:33 +0100

    feat(about-us): finalise portrait view [#284]

--- components/pages/MemberPortrait.tsx
+++ components/pages/MemberPortrait.tsx
@@ -1,7 +1,19 @@
-import { ArrowRightIcon, BadgeCheckIcon } from '@heroicons/react/outline';
 import { FC } from 'react';
+import { LinkIcon, ArrowRightIcon, CheckBadgeIcon, UserGroupIcon } from '@heroicons/react/24/outline';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faApple,
+	faFacebook,
+	faInstagram,
+	faLinkedin,
+	faSpotify,
+	faTwitter,
+	faXing,
+} from '@fortawesome/free-brands-svg-icons';
+
 import { getTeamMemberPortrait } from '../../api/team';
 import { useTranslation } from '../../hooks/useTranslation';
+import { formatDate } from '../../utils/date';
 import { ContentConstraint } from '../ContentConstraint';
 import { Heading } from '../Heading';
 import { Image } from '../Image';
@@ -17,8 +29,45 @@ export interface MemberPortraitPageComponentProps {
 	favoriteDrink?: string | null;
 	favoriteGenre?: string | null;
 	bio?: string | null;
+	entryDate?: string | null;
 	links?: Awaited<ReturnType<typeof getTeamMemberPortrait>>['links'];
 }
+
+const getGuessedLinkIcon = (link: { url?: string | null; label?: string | null }) => {
+	if (!link.url || !link.label) {
+		return;
+	}
+
+	if (link.url.includes('instagram.com')) {
+		return <FontAwesomeIcon icon={faInstagram} />;
+	}
+
+	if (link.url.includes('facebook.com')) {
+		return <FontAwesomeIcon icon={faFacebook} />;
+	}
+
+	if (link.url.includes('twitter.com')) {
+		return <FontAwesomeIcon icon={faTwitter} />;
+	}
+
+	if (link.url.includes('spotify.com')) {
+		return <FontAwesomeIcon icon={faSpotify} />;
+	}
+
+	if (link.url.includes('music.apple.com')) {
+		return <FontAwesomeIcon icon={faApple} />;
+	}
+
+	if (link.url.includes('linkedin.com')) {
+		return <FontAwesomeIcon icon={faLinkedin} />;
+	}
+
+	if (link.url.includes('xing.com')) {
+		return <FontAwesomeIcon icon={faXing} />;
+	}
+
+	return <LinkIcon className="inline-block h-4 align-middle" />;
+};
 
 export const MemberPortraitPageComponent: FC<MemberPortraitPageComponentProps> = ({
 	image,
@@ -26,6 +75,7 @@ export const MemberPortraitPageComponent: FC<MemberPortraitPageComponentProps> =
 	favoriteDrink,
 	favoriteGenre,
 	bio,
+	entryDate,
 	isFounder = false,
 	links,
 	primaryActivityArea,
@@ -41,15 +91,24 @@ export const MemberPortraitPageComponent: FC<MemberPortraitPageComponentProps> =
 							<Image isObjectFitCover src={image?.path || '#'} alt={name} className="rounded-full" />
 						</div>
 						<div className="mt-8 md:mt-0 md:ml-8">
-							<Heading level="1">{name}</Heading>
-							<p className="text-gray-600">
+							<p className="text-gray-600 mb-1">
 								<span>
 									{isFounder ? 'Migründer' : 'Teammitglied'}
+									{!isFounder && (
+										<UserGroupIcon className="inline-block align-text-top text-sea-green-400 h-5 w-5 ml-1" />
+									)}
 									{isFounder && (
-										<BadgeCheckIcon className="inline-block align-text-top text-alpine-green-400 h-5 w-5 ml-1" />
+										<CheckBadgeIcon className="inline-block align-text-top text-alpine-green-400 h-5 w-5 ml-1" />
 									)}
 								</span>
 							</p>
+							<Heading level="1">{name}</Heading>
+							{entryDate && (
+								<p className="text-gray-300 text-sm">
+									Beigetreten am {''}
+									{formatDate(entryDate)}
+								</p>
+							)}
 						</div>
 					</header>
 					{bio && (
@@ -58,44 +117,53 @@ export const MemberPortraitPageComponent: FC<MemberPortraitPageComponentProps> =
 							<p className="mt-2 text-lg md:w-5/6">{bio}</p>
 						</div>
 					)}
+					<hr className="mt-8 h-px bg-gray-100 border-0" />
 					<div className="mt-8">
 						<ul>
 							<li className="mb-8">
-								<Heading level="4">Mein Arbeitsbereich</Heading>
+								<Heading level="5">Was ist dein Arbeitsbereich?</Heading>
 								<p className="mt-2 text-lg">{translate(`team.roles.${primaryActivityArea}`)}</p>
 							</li>
 							{favoriteGenre && (
 								<li className="mb-8">
-									<Heading level="4">Mein Heimgenre</Heading>
+									<Heading level="5">In welchem Genre fühlst du dich zuhause?</Heading>
 									<p className="mt-2 text-lg">{favoriteGenre}</p>
 								</li>
 							)}
 							{favoriteDrink && (
 								<li>
-									<Heading level="4">Mein Lieblingsgetränk ist</Heading>
+									<Heading level="5">Welches Getränk bestellst du dir an der Bar?</Heading>
 									<p className="mt-2 text-lg">{favoriteDrink}</p>
 								</li>
 							)}
 						</ul>
 					</div>
 					{links && (
-						<div className="mt-8">
-							<Heading level="4">Weiterführende Links</Heading>
-							<ul className="mt-2">
-								{links.map((link) => (
-									<li key={link?.value?.url} className="my-1">
-										<Link
-											isStandalone
-											className="text-sea-green-500"
-											href={link?.value?.url || '#'}
-											icon={<ArrowRightIcon className="ml-1 h-5 inline-block" />}
-										>
-											{link?.value?.label}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</div>
+						<>
+							<hr className="mt-8 h-px bg-gray-100 border-0" />
+							<div className="mt-8">
+								<Heading level="4">Weiterführende Links</Heading>
+								<ul className="mt-2">
+									{links.map((link) => (
+										<li key={link?.value?.url} className="my-1">
+											<Link
+												isStandalone
+												className="text-sea-green-500"
+												href={link?.value?.url || '#'}
+											>
+												<span className="inline-block w-5 mr-1">
+													{getGuessedLinkIcon({
+														url: link?.value?.url,
+														label: link?.value?.label,
+													})}
+												</span>
+												{link?.value?.label}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</div>
+						</>
 					)}
 				</article>
 			</ContentConstraint>
